f01c10dd333618a09b99d04a8370b411f071e1aa
Paul.Graton <paulgtn@hotmail.fr>
2019-01-17 21:02:38 +0100

    Spec Ville ok

--- spec/ville_mail_spec.rb
+++ spec/ville_mail_spec.rb
@@ -1,7 +1,14 @@
 require_relative '../lib/ville_mail'
 
+describe "the perform method" do
+it "should return at least 185 cities" do
+  expect(perform.length).to eq(185) 
+end
 
-describe "the get_the_url method" do
-      expect(municipalities[0]).to eq("ABLEIGES"=>"mairie.ableiges95@wanadoo.fr")
-    end
-  
+it "should include MUNICIPALITY => name@email" do
+expect(perform[0]).to eq("ABLEIGES"=>"mairie.ableiges95@wanadoo.fr")
+expect(perform[10]).to eq("ATTAINVILLE"=>"mairie@attainville.fr")
+expect(perform[15]).to eq("BEAUCHAMP"=>"accueil-mairie@ville-beauchamp.fr")
+end
+end
+
